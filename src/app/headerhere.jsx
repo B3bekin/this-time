d8ddaf58b635b165aa16d307b1afddb423ba6871
B3bekin <92512370+B3bekin@users.noreply.github.com>
2023-05-19 12:19:40 +0000

    zesty

---
+++ src/app/headerhere.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Header() {
+
+    return (
+        <div className="bg-white flex justify-evenly items-center">
+
+            <div className="font-extrabold text-4xl flex justify-end flex-col text-[#FF4A4A]">
+                <h1 className="bg-green-100">SVEZ</h1>
+                <h1 className="bg-green-200 max-w-fit">.SE</h1>
+            </div>
+
+            <ul className="text-2xl font-light">
+                Aplikace
+            </ul>
+
+            <ul className="text-2xl font-light">
+                Pro ridice
+            </ul>
+
+            <ul className="text-2xl font-light">
+                Kontakty
+            </ul>
+
+            <p>🎈🔽</p>
+
+            <div className="bg-[#FF4A4A] text-white py-2 px-5 rounded-md text-center flex align-bottom">Objednat jízdu</div>
+
+        </div>
+
+    )
+}
+
+export default Header
