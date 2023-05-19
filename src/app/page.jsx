33938effbc23a9dce9ddf7a5af42416aef076a6c
B3bekin <92512370+B3bekin@users.noreply.github.com>
2023-05-19 11:18:10 +0000

    please

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,9 +3,30 @@ import React from "react";
 function Home() {
 
   return(
-    <div>
-      <div>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta enim illum modi at suscipit maxime iste odit ex quasi, aspernatur distinctio esse beatae nulla eaque iure sit minima voluptatem vero.
+    <div className="bg-violet-300 min-h-screen">
+      <div className="bg-white flex justify-evenly items-center">
+
+        <div className="font-extrabold text-4xl flex flex-col justify-end text-[#FF4A4A]">
+          <h1>SVEZ</h1>
+          <h1>.SE</h1>
+        </div>
+
+        <ul className="text-xl font-light">
+          Aplikace
+        </ul>
+
+        <ul>
+          Pro ridice
+        </ul>
+
+        <ul>
+          Kontakty
+        </ul>
+        
+        <p>🎈🔽</p>
+
+          <div className="bg-[#FF4A4A] text-white py-2 px-5 rounded-md text-center flex align-bottom">Objednat jízdu</div>
+
       </div>
 
     </div>
